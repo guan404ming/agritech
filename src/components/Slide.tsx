c995df0c6facf9442c5e542c39639f38979a9d8e
guan404ming <guanmingchiu@gmail.com>
2023-01-21 16:39:24 +0800

    feat: add slide

---
+++ src/components/Slide.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+function Slide() {
+    return (
+        <>
+            <h1 className="font-bold text-lg my-6">今日漲跌最大</h1>
+            <div className="carousel w-full h-1/20 space-x-4 pb-4">
+
+                <div id="slide1" className="carousel-item w-1/3 md:w-1/3 lg:w-1/5 min-w-[200px]">
+                    <div className="stats shadow-lg relative w-full">
+                        <div className="text-secondary absolute top-11 right-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" /></svg>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">紅豆</div>
+                            <div className="stat-value my-1 text-2xl">89,400</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div id="slide1" className="carousel-item w-1/3 md:w-1/3 lg:w-1/5 min-w-[200px]">
+                    <div className="stats shadow-lg relative w-full">
+                        <div className="text-secondary absolute top-11 right-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" /></svg>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">紅豆</div>
+                            <div className="stat-value my-1 text-2xl">89,400</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div id="slide1" className="carousel-item w-1/3 md:w-1/3 lg:w-1/5 min-w-[200px]">
+                    <div className="stats shadow-lg relative w-full">
+                        <div className="text-secondary absolute top-11 right-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" /></svg>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">紅豆</div>
+                            <div className="stat-value my-1 text-2xl">89,400</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div id="slide1" className="carousel-item w-1/3 md:w-1/3 lg:w-1/5 min-w-[200px]">
+                    <div className="stats shadow-lg relative w-full">
+                        <div className="text-secondary absolute top-11 right-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" /></svg>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">紅豆</div>
+                            <div className="stat-value my-1 text-2xl">89,400</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div id="slide1" className="carousel-item w-1/3 md:w-1/3 lg:w-1/5 min-w-[200px]">
+                    <div className="stats shadow-lg relative w-full">
+                        <div className="text-secondary absolute top-11 right-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" /></svg>
+                        </div>
+                        <div className="stat">
+                            <div className="stat-title">紅豆</div>
+                            <div className="stat-value my-1 text-2xl">89,400</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+export default Slide;
