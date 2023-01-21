1ecbc92f1bdb1b2d990aec8f9482ec5f9b9cc956
guan404ming <guanmingchiu@gmail.com>
2023-01-21 16:38:47 +0800

    fix: adjust searchbar position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,12 @@ function Header() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <button className="btn btn-ghost normal-case text-xl" type="button">Agritech</button>
+                <button className="btn btn-ghost normal-case text-2xl font-bold" type="button">Agritech</button>
             </div>
             <div className="flex-none">
+                <button className="btn btn-ghost btn-circle" type="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button>
                 <button className="btn btn-ghost btn-circle" type="button">
                     <div className="indicator">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
